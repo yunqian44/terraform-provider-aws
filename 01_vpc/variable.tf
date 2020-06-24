ed176ea5eed710b68580df83195b531f23ebfdd2
yunqian44 <yunqian8@live.com>
2020-06-24 15:41:57 +0800

    modify

--- 01_vpc/variable.tf
+++ 01_vpc/variable.tf
@@ -17,10 +17,10 @@ variable "subnet_cidr"{
   default = ["190.160.1.0/24","190.160.2.0/24","190.160.3.0/24"]
 }
 
-variable "azs"{
-  type = list(string)
-  default = ["us-west-2a","us-west-2b","us-west-2c"]
-}
+# variable "azs"{
+#   type = list(string)
+#   default = ["us-west-2a","us-west-2b","us-west-2c"]
+# }
 
 data "aws_availability_zones" "azs" {
 
